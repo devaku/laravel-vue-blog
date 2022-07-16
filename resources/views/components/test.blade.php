95511b8bd115dc267da3f4f6cfa351a4746a60a3
devaku <alejokimuy@gmail.com>
2022-07-16 09:52:29 +0800

    testing

---
+++ resources/views/components/test.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <style>
+            body {
+                font-family: 'Nunito', sans-serif;
+            }
+        </style>
+    </head>
+    <body class="antialiased">
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum, ipsa, alias, sint cumque enim ad dolorum inventore illo doloribus magni sunt maiores consectetur iusto suscipit deserunt nulla. Possimus, quae praesentium?</p>
+    </body>
+</html>
---
+++ resources/views/testview.blade.php
@@ -0,0 +1,8 @@
+<x-test>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis quisquam aliquid rerum voluptatum, corrupti doloremque accusantium, cumque facere impedit pariatur alias quia inventore, ducimus commodi quo reprehenderit molestiae consectetur? Voluptatum!</p>
+</x-test>
