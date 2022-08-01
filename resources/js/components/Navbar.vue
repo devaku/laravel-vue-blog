e4a39db0f85360969380a2e7a288f6886774c6c2
devaku <alejokimuy@gmail.com>
2022-08-01 11:26:22 +0800

    Added login / logout in navbar

--- resources/js/components/Navbar.vue
+++ resources/js/components/Navbar.vue
@@ -1,19 +1,101 @@
 <template>
-    <nav>
-        <router-link to="/" exact>Home</router-link> |
-
-        <router-link v-if="currentRouteName !== 'login'" to="/login" exact
-            >Login</router-link
-        >
-        |
-        <router-link to="/asdfasdf" exact>404</router-link>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
+        <div class="container-fluid">
+            <router-link to="/" exact class="navbar-brand">Home</router-link>
+            <button
+                class="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+            >
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <!-- Left side -->
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0"></ul>
+                <!-- Right side -->
+                <ul class="navbar-nav mb-2 mb-lg-0">
+                    <li class="nav-item" v-if="isLoggedIn">
+                        <a href="#" class="nav-link" v-on:click="submitForm()"
+                            >Logout</a
+                        >
+                    </li>
+                    <li class="nav-item" v-else>
+                        <span v-if="currentRouteName !== 'login'">
+                            <router-link to="/login" exact class="nav-link"
+                                >Login</router-link
+                            >
+                        </span>
+                    </li>
+                </ul>
+            </div>
+        </div>
     </nav>
 </template>
 <script>
 export default {
+    data() {
+        return {
+            logoutUrl: `${process.env.MIX_APP_URL}/logout`,
+            isLoggedIn: null,
+        };
+    },
+
+    beforeMount() {
+        // this.loginStatus();
+    },
+
+    created() {
+        this.loginStatus();
+    },
+
+    mounted() {
+        // this.loginStatus();
+    },
+
     computed: {
         currentRouteName() {
             return this.$route.name;
+        },
+    },
+
+    methods: {
+        async loginStatus() {
+            let response = await axios
+                .get("/api/authenticated")
+                .then(() => {
+                    return true;
+                })
+                .catch(() => {
+                    return false;
+                });
+
+            this.isLoggedIn = response;
+        },
+        async submitForm() {
+            // this.$refs.logoutForm.submit();
+            await axios(this.logoutUrl, {
+                method: "post",
+                data: {},
+                headers: {
+                    // 'Content-Type': 'multipart/form-data',
+                    Accept: "application/json",
+                },
+            })
+                .then((response) => {
+                    if (this.currentRouteName != "home") {
+                        this.$router.push({ name: "home" });
+                    } else {
+                        // Refresh the page
+                        this.$router.go();
+                    }
+                })
+                .catch((e) => {
+                    console.log(e);
+                });
         },
     },
 };
