50ab8aa649f0dd20c01cff78bed37ccbcce28e26
devaku <alejokimuy@gmail.com>
2022-08-01 11:27:13 +0800

    Added Login Page

--- resources/js/pages/dashboard/Login.vue
+++ resources/js/pages/dashboard/Login.vue
@@ -1,12 +1,93 @@
 <template>
     <Navbar></Navbar>
-    <div>LOGIN PAGE</div>
+    <div class="container error-holder my-3" v-if="formError">
+        <div class="card border-danger mb-3">
+            <div class="card-header text-danger border-danger">Error</div>
+            <div class="card-body">
+                <p class="card-text">{{ formError }}</p>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <form @submit.prevent="submitForm">
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">
+                    Email address</label
+                >
+                <input
+                    type="email"
+                    class="form-control"
+                    id="exampleInputEmail1"
+                    aria-describedby="emailHelp"
+                    v-model="email"
+                />
+                <div id="emailHelp" class="form-text">
+                    We'll never share your email with anyone else.
+                </div>
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">
+                    Password</label
+                >
+                <input
+                    type="password"
+                    class="form-control"
+                    id="exampleInputPassword1"
+                    v-model="password"
+                />
+            </div>
+            <button type="submit" class="btn btn-primary w-100">Submit</button>
+        </form>
+    </div>
 </template>
 <script>
+import axios from "axios";
 import Navbar from "../../components/Navbar.vue";
 export default {
     components: {
         Navbar,
     },
+
+    data() {
+        return {
+            formError: null,
+            email: null,
+            password: null,
+        };
+    },
+
+    methods: {
+        async submitForm() {
+            this.formErrors = [];
+            const data = {
+                email: this.email,
+                password: this.password,
+            };
+
+            const url = `${process.env.MIX_APP_URL}/login`;
+            const response = await axios(url, {
+                method: "post",
+                data,
+                headers: {
+                    // 'Content-Type': 'multipart/form-data',
+                    Accept: "application/json",
+                },
+            })
+                .then((response) => {
+                    this.$router.push({ name: "home" });
+                })
+                .catch((e) => {
+                    // console.log(JSON.stringify(e));
+                    console.log(JSON.stringify(e.response));
+                    this.formError = e.response.data.message;
+                });
+        },
+    },
 };
 </script>
+
+<style scoped lang="scss">
+.container {
+    max-width: 500px;
+}
+</style>
