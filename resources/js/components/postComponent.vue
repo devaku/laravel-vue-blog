5b0058495aaaed7ef93e876ed8394d3f92b299d3
devaku <alejokimuy@gmail.com>
2022-08-01 11:26:58 +0800

    Added index and PostComponent

---
+++ resources/js/components/postComponent.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="card card-body mb-2" v-for="post in posts" :key="post.id">
+        <h3>{{ post.title }}</h3>
+        <p>{{ post.body }}</p>
+        <hr />
+        <button @click="editArticle(post)" class="btn btn-warning mb-2">
+            Edit
+        </button>
+        <button @click="deleteArticle(post.id)" class="btn btn-danger">
+            Delete
+        </button>
+    </div>
+</template>
+<script>
+export default {
+    props: ["posts"],
+};
+</script>
+<style></style>
--- resources/js/pages/index.vue
+++ resources/js/pages/index.vue
@@ -1,11 +1,125 @@
 <template>
     <Navbar></Navbar>
-    <div>INDEX PAGE</div>
+    <div class="posts-form container">
+        <h2>Share what's on your mind.</h2>
+        <form @submit.prevent="createPost" action="" class="mb-3">
+            <div class="form-group mb-3">
+                <input
+                    type="text"
+                    class="form-control"
+                    placeholder="Title"
+                    v-model="post.title"
+                />
+            </div>
+            <div class="form-group mb-3">
+                <textarea
+                    class="form-control"
+                    name="body"
+                    id=""
+                    rows="3"
+                    placeholder="Body"
+                    v-model="post.body"
+                ></textarea>
+            </div>
+
+            <div class="d-grid">
+                <button type="submit" class="btn btn-primary">Save</button>
+            </div>
+        </form>
+    </div>
+    <!-- Pagination -->
+    <div class="container my-5">
+        <ul class="pagination justify-content-center">
+            <li
+                :class="[{ disabled: !pagination.prev_page_url }]"
+                class="page-item"
+                @click="
+                    pagination.prev_page_url &&
+                        fetchPosts(pagination.prev_page_url)
+                "
+            >
+                <a class="page-link" href="#">Previous</a>
+            </li>
+            <li class="page-item disabled">
+                <a class="page-link text-dark" href="#"
+                    >Page {{ pagination.current_page }} of
+                    {{ pagination.last_page }}</a
+                >
+            </li>
+            <li
+                :class="[{ disabled: !pagination.next_page_url }]"
+                class="page-item"
+                @click="
+                    pagination.next_page_url &&
+                        fetchPosts(pagination.next_page_url)
+                "
+            >
+                <a class="page-link" href="#">Next</a>
+            </li>
+        </ul>
+    </div>
+    <!-- Posts Content -->
+    <div class="container">
+        <postComponent :posts="posts" v-if="posts.length > 0"></postComponent>
+        <div v-else>
+            <h2 class="text-center">There are no posts available</h2>
+        </div>
+    </div>
 </template>
 <script>
+import axios from "axios";
 import Navbar from "../components/Navbar.vue";
+import postComponent from "../components/postComponent.vue";
+
 export default {
-    components: { Navbar },
+    data() {
+        return {
+            post: {
+                title: null,
+                body: null,
+            },
+            posts: [],
+            pagination: {},
+        };
+    },
+    mounted() {
+        this.fetchPosts();
+    },
+    components: { Navbar, postComponent },
+    methods: {
+        async fetchPosts(pageUrl) {
+            let fetchUrl = pageUrl
+                ? pageUrl
+                : `${process.env.MIX_APP_URL}/api/posts`;
+
+            axios
+                .get(fetchUrl)
+                .then((response) => response.data)
+                .then((response) => {
+                    this.posts = response.data;
+                    // this.posts = [];
+                    this.makePagination(response.meta, response.links);
+                })
+                .catch((e) => {
+                    console.log(e);
+                });
+        },
+
+        makePagination(meta, links) {
+            this.pagination = {
+                current_page: meta.current_page,
+                last_page: meta.last_page,
+                next_page_url: links.next,
+                prev_page_url: links.prev,
+            };
+        },
+
+        async createPost() {},
+    },
 };
 </script>
-<style scoped></style>
+<style scoped>
+.posts-form {
+    max-width: 500px;
+}
+</style>
