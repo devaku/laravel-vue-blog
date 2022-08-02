488a448fb2fa03058f0df2488ebd8307112955aa
devaku <alejokimuy@gmail.com>
2022-08-02 15:12:12 +0800

    Added some testing in the blade page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,9 +3,24 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}"/>
         <title>Laravel-Vue-Blog</title>
     </head>
     <body>
+    {{-- @if (Auth::check())
+    <script>
+        window.Laravel = {!!json_encode([
+               'isLoggedin' => true,
+               'user' => Auth::user()
+           ])!!}
+    </script>
+    @else
+        <script>
+            window.Laravel = {!!json_encode([
+                    'isLoggedin' => false
+                ])!!}
+        </script>
+    @endif --}}
         <div id="app"></div>
     </body>
     <script src="{{mix('js/app.js')}}"></script>
