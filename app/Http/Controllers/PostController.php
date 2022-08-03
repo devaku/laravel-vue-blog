e989f4716275baef359e16a051d673e3360859d7
devaku <alejokimuy@gmail.com>
2022-08-03 10:51:28 +0800

    Fixed routes in PostController

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,14 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Resources\PostResource;
+use App\Http\Resources\PostCollection;
 use App\Models\Post;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 
 class PostController extends Controller
 {
 
     public function __construct()
     {
-        $this->middleware('api');
+        // $this->middleware('web');
         $this->middleware('auth')->only(['create', 'store', 'edit', 'update', 'destroy']);
     }
     /**
@@ -58,6 +61,13 @@ class PostController extends Controller
         return new PostResource($post);
     }
 
+    public function showBasedOnUser(Request $request)
+    {
+        $userid = Auth::user()->id;
+        $post = Post::where('user_id', $userid)->get();
+        return new PostCollection($post);
+    }
+
     /**
      * Show the form for editing the specified resource.
      *
@@ -79,7 +89,18 @@ class PostController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $post = Post::findOrFail($id);
+        $post->title = $request->input('title');
+        $post->body = $request->input('body');
+        
+        if($post->save()){
+            return new PostResource($post);
+        }
+        else {
+            return response('Error in updating the post!', 400)
+            ->header('Content-Type', 'text/plain');
+        }
+        
     }
 
     /**
@@ -93,3 +114,4 @@ class PostController extends Controller
         //
     }
 }
+
